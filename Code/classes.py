a3f05074571cd922861445fec636468859050dd2
jeff Cate <Jeff.a.cate@gmail.com>
2020-07-30 12:01:59 -0500

    python screen clear function imported for cleaner ui.

--- Code/classes.py
+++ Code/classes.py
@@ -1,8 +1,13 @@
 import sys
+import os
 
 c195 = list()
 c197 = list()
 both = dict()
+
+# import cls and clear
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
 
 def fillBoth(comm1, techReporting):
     for name in comm1:
@@ -15,41 +20,27 @@ def fillBoth(comm1, techReporting):
                 except Exception as e:
                     print(e.__class__.__name__, e)
 def getName():
-    name = ""
-    try:
-        raise NotImplementedError('Name ')
-    except Exception as e:
-        print(e.__class__.__name__, e)
+    input('ClearThis')
+    cls()
+    name = input('Instructors Name: ')
     return name
 
 def getNames(cName):
     done = False
-    while done == False:
-        name = getName()
-        if cName in both.keys():
-            continue
-        else:
-            both.update({cName,name})
+    while done != True:
+        getName()
 
-def getCourse(cName):
-    
-    res = input('Would you like to add another class? (\'ClassName\', \'n\')')
-    if res.lower() == 'done':
-            done = True
-        elif res.lower() == '195':
-            getNames(res)
-        elif res.lower() == '197':
-            getNames(res)
-        else : print('Invalid course selection! Please try again!')
+def getCourse():
+    try:
+        raise NotImplementedError('getCourse(cName)')
+    except Exception as e:
+        print(e.__class__.__name__, e)
 
 def runProgram():
-    done = False
-    res = input('Would you like to add another class?  \n\t<course name> -or- done: ')
-    while done == False:
-        # SET Class
-        course = getCourse()
-        # GET Instructors for Class
-        getNames(course)
-        else : print('Invalid course selection! Please try again!')
+    try:
+        raise NotImplementedError('runProgram()')
+    except Exception as e:
+        print(e.__class__.__name__, e)
 
-runProgram()
+res = getName()
+print(res)
