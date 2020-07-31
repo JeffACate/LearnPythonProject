64a3937a50f948575de4604203711dd664ebef54
jeff Cate <Jeff.a.cate@gmail.com>
2020-07-31 13:03:19 -0500

    UI Prefernce fixes.

--- Code/classes.py
+++ Code/classes.py
@@ -1,44 +1,82 @@
 import sys
 import os
 
-c195 = list()
-c197 = list()
-both = dict()
+class CourseComparer:
 
-# import cls and clear
-def cls():
-    os.system('cls' if os.name=='nt' else 'clear')
+    c195 = list()
+    c197 = list()
+    both = list()
 
-def fillBoth(comm1, techReporting):
-    for name in comm1:
-        if name in techReporting:
-            if name in both:
-                continue
+    # import cls and clear
+    def cls(self):
+        os.system('cls' if os.name=='nt' else 'clear')
+
+    #SINGLE RESPONSIBILITY: gets a NAME for a specified COURSE
+    def GetName(self,cName):
+        name = input('Instructors Name for {0}: '.format(cName))
+        return name
+
+    #SINGLE RESPONSIBILITY: makes a LIST of instructors for a specific COURSE
+    def GetNames(self,cName):
+        done = False
+        name = ''
+        while done != True:
+            # self.cls()
+            if cName == 'c195':
+                print(self.c195)
             else:
-                try:
-                    raise NotImplementedError('Name ')
-                except Exception as e:
-                    print(e.__class__.__name__, e)
-def getName(cName):
-    name = input('Instructors Name for {0}: '.format(cName))
-    return name
+                print(self.c197)
+            if name.lower() == 'done':
+                done = True
+            else:
+                name = self.GetName(cName)
+                if name != 'done':
+                    if cName == 'c195':
+                        if name not in self.c195:
+                            self.c195.append(name)
+                    elif cName == 'c197':
+                        if name not in self.c197:
+                            self.c197.append(name)
+                else:
+                    print('Invalid response. Try again.')
 
-def getNames(cName):
-    done = False
-    while done != True:
-        getName(cName)
+    def GetCourse(self):
+        invalid = False
+        while invalid != True:
+            # self.cls()
+            done = input('\n(0) exit\n(1) 195\n(2) 197\nWhat class would you like to select? ')
+            if done == '0':
+                invalid = True
+            elif done == '1':
+                self.GetNames('c195')
+            elif done == '2':
+                self.GetNames('c197')
+            else:
+                print('Invalid response. Try again.')
+    
+    def CompareCourses(self,course1,course2):
+        newList = list()
+        for instructor in course1:
+            if instructor in course2:
+                if instructor in newList:
+                    continue
+                else:
+                    print(instructor)
+                    newList.append(instructor)
+        for instructor in course2:
+            if instructor in course1:
+                if instructor in newList:
+                    continue
+                else:
+                    print(instructor)
+                    newList.append(instructor)
+        return newList
 
-def getCourse():
-    try:
-        raise NotImplementedError('getCourse(cName)')
-    except Exception as e:
-        print(e.__class__.__name__, e)
+    def RunProgram(self):
+        self.GetCourse()
+        self.both = self.CompareCourses(self.c195,self.c197)
+        return self.both
 
-def runProgram():
-    try:
-        raise NotImplementedError('runProgram()')
-    except Exception as e:
-        print(e.__class__.__name__, e)
-
-res = getName('c195')
-print(res)
+c = CourseComparer()
+c.RunProgram()
+print('\n195:',c.c195, '\n197:', c.c197,'\nboth:', c.both)
