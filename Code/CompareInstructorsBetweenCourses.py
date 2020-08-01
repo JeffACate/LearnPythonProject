fb803f9dede8dbde7dab0f68498b8fb00991898c
JeffACate <Jeff.A.Cate@gmail.com>
2020-08-01 16:32:04 -0500

    VS Code settings updated

--- Code/CompareInstructorsBetweenCourses.py
+++ Code/CompareInstructorsBetweenCourses.py
@@ -76,7 +76,6 @@ class InsturctorComparer:
         self.GetCourse()
         self.both = self.CompareCourses(self.c195,self.c197)
         return self.both
-
 c = InsturctorComparer()
 c.RunProgram()
 print('\n195:',c.c195, '\n197:', c.c197,'\nboth:', c.both)
